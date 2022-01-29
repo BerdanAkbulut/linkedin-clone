5d90a0ffae21b58a51979eec465f97b63a9b02c6
BerdanAkbulut <berdanakbulutt@hotmail.com>
2022-01-29 20:35:49 +0300

    hidden sm newss

--- components/Widgets.js
+++ components/Widgets.js
@@ -7,7 +7,7 @@ function Widgets({ articles }) {
   const truncate = (string, n) =>
     string?.length > n ? string.substr(0, n - 1) + "..." : string;
   return (
-    <div className="relative max-w-lg xl:h-1/3 md:h-1/3 sm:h-1/3 xs:h-1/3 overflow-hidden  dark:bg-gray-800/80 py-2 px-2 text-md font-medium border border-gray-400 dark:border-none rounded-lg dark:text-white/75  text-black ">
+    <div className="  sm:hidden md:hidden relative max-w-lg xl:h-1/3 md:h-1/3 sm:h-1/3 xs:h-1/3 overflow-hidden  dark:bg-gray-800/80 py-2 px-2 text-md font-medium border border-gray-400 dark:border-none rounded-lg dark:text-white/75  text-black ">
       <h3 className="text-center text-lg font-semibold text-white mb-1">
         LinkedIn News
       </h3>
@@ -23,7 +23,7 @@ function Widgets({ articles }) {
           />
         </div>
       ))}
-      <div className= " sm:hidden bg-white mt-5 dark:bg-transparent  w-11/12 h-64 px-2.5  rounded-lg sticky top-10 border border-gray-300 dark:border-none">
+      <div className= "  bg-white mt-5 dark:bg-transparent  w-11/12 h-64 px-2.5  rounded-lg sticky top-10 border border-gray-300 dark:border-none">
         <div className="relative w-full h-full ">
           <Image
             src="https://rb.gy/kbfeaa"
